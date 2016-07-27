54b25fc0458803cba4081963c37096c9b46aabe5
ondercalbay <ondercalbay@hotmail.com>
2016-07-27 23:16:15 +0300

    Api

---
+++ TangentIdeas.Mail.Api/Controllers/HomeApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TangentIdeas.Mail.Api.Controllers
+{
+    [RoutePrefix("api/homeapi")]
+    public class HomeApiController:ApiController
+    {
+        [HttpGet,Route("hello")]
+        public string Hello() {
+        return "Hello";
+        }
+    }
+}
---
+++ TangentIdeas.Mail.Api/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangentIdeas.Mail.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            var webApp = WebApp.Start<Startup>(url: baseAddress);
+
+            Console.ReadLine();
+        }
+    }
+}
