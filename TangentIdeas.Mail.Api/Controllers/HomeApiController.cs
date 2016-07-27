14e531dfd99a28a00b58c06d4c00b6b003af7be7
omer-demir <farukomer.demir@hepsiburada.com>
2016-07-27 23:56:56 +0300

    changes

--- TangentIdeas.Mail.Api/Controllers/HomeApiController.cs
+++ TangentIdeas.Mail.Api/Controllers/HomeApiController.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Tangent.CeviriDukkani.Data.Model;
+using Tangent.CeviriDukkani.WebCore.BaseControllers;
 
-namespace TangentIdeas.Mail.Api.Controllers
-{
+namespace TangentIdeas.Mail.Api.Controllers {
     [RoutePrefix("api/homeapi")]
-    public class HomeApiController:ApiController
-    {
-        [HttpGet,Route("hello")]
+    public class HomeApiController : BaseApiController {
+        private readonly CeviriDukkaniModel _model;
+
+        public HomeApiController() {
+            _model = new CeviriDukkaniModel();
+        }
+
+        [HttpGet, Route("hello")]
         public string Hello() {
-        return "Hello";
+            return "Hello";
+        }
+
+        [HttpPost, Route("sendMails")]
+        public HttpResponseMessage SendMails([FromBody]SendMailRequestDto sendMailRequest) {
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+           
+
+
+            return result;
         }
     }
 }
---
+++ TangentIdeas.Mail.Api/Controllers/SendMailRequestDto.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace TangentIdeas.Mail.Api.Controllers {
+    public class SendMailRequestDto {
+        public List<string> MailAddress { get; set; }
+    }
+}
